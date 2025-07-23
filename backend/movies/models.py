95f4ce30b97351dd7c2cb2ff9dc51f1b0a91ee82
Aswin-AR5055 <aswinraj2003@gmail.com>
2025-07-23 15:28:18 +0530

    added several features

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
+    rating = models.FloatField(default=0.0)
     description = models.TextField()
     poster = models.URLField()
 
